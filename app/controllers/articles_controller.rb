e7b3afca749029c428ee893bd0a8eb443b4310d6
Bang Nguyen <bangnv77@gmail.com>
2016-09-09 09:56:30 -0700

    add new & create controller actions and views. And also add an error message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     render :show
   end
+
+  def new
+    @article = Article.new
+    render :new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :author, :points)
+  end
 end
