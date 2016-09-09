da9395b130676640e724025afb4c2346951d73b5
Bang Nguyen <bangnv77@gmail.com>
2016-09-09 12:01:49 -0700

    create delete controller action and view for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,6 +30,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@comment.article)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text, :user, :article_id)
