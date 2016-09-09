0f1d295c468caa27f869abd558a8b8c6513fbaa4
Bang Nguyen <bangnv77@gmail.com>
2016-09-09 11:56:46 -0700

    create edit & update controller actions and views for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to article_path(@comment.article)
+    else
+      render :edit
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text, :user, :article_id)
