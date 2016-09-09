580e2cadfc5d0911875ef9b8d1aac07b687e164e
Bang Nguyen <bangnv77@gmail.com>
2016-09-09 10:14:48 -0700

    create edit & update controller actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :author, :points)
